9194372c95ac8a6264ee5234ae5935671aef2546
Sanggyun Lee <s4054367@student.rmit.edu.au>
2025-05-22 22:46:42 +1000

    10:46 22/05/2025

--- src/game/core/GameLogic.java
+++ src/game/core/GameLogic.java
@@ -1,21 +1,24 @@
 package game.core;
 
-import game.world.Location;
-
-import java.util.Scanner;
-
+import game.entity.Enemy;
 import game.entity.Player;
+import game.logic.Combat;
+import game.logic.Navigation;
+import game.world.Location;
+import java.util.Scanner;
 
 /**
  * GameLogic.java's purpose is to manage the game flow, and to manage the
  * backbone logic of the game such as user input, game loop, game start/end
  * it essentially controls the states of the games.
  * 
- * @seanlee1991
+ * @author Sanggyun Lee
  */
 public class GameLogic {
+
     private Location[][] map;
     private Player player;
+    private final int MAP_SIZE = 5; // map size not going to change
 
     public void start() {
         map = generateMap();
@@ -26,8 +29,8 @@ public class GameLogic {
 
 
 
-        while (true) {
-            System.out.println("Command (n/e/s/w/q): ");
+        while (player.isAlive()) {
+            System.out.println("Command (n/e/s/w/q/map/hp/i): ");
             String userInput = scanner.nextLine().toLowerCase();
 
             if (userInput.equals("q")) {
@@ -41,9 +44,144 @@ public class GameLogic {
             } else if (userInput.equals("i")) {
                 printInventory();
             } else {
-                //here is where the navigation logic goes
+                // Here is where the navigaion logic goes.
+                if (!Navigation.isValidMove(player, userInput, MAP_SIZE)) {
+                    System.out.println("You can't move out of the bounds");
+                    continue; //so we can keep run the while loop
+                }
+
+                int[] nextIntendedPosition = Navigation.getNewPosition(player, userInput);
+                // int[row,col] so we have to type 0,1
+                int newRow = nextIntendedPosition[0]; //extract the data
+                int newCol = nextIntendedPosition[1];
+                //this is to check your location logic
+                Location newLocation = map[newRow][newCol];
+                if (newLocation.isImpossible()) {
+                    System.out.println("This area not exist. Try another direction.");
+                    continue; // we have to keep run to get correct input from the user
+                }
+
+                if (player.getHp() < 0.0 ) {
+                    System.out.println("You died. Game over.");
+                    break; // If player died, end game
+                }
                 
+                //this is where the player moves to the new location
+                player.setPosition(newRow, newCol);
+                
+                System.out.println("You have entered: " + newLocation.getName());
+
+                //check the itmes we obtain
+                giveItemIfAvailable(newLocation.getName().toLowerCase());
+                
+                //we need to check that the new location has enermy or not
+                //if there is enemy we have to give the information about the enemy and combat
+                if (newLocation.hasEnermy()) {
+                    Enemy enemy = newLocation.getEnermy();
+                    //engage with the enemy
+                    boolean survived = Combat.engage(player, enemy, scanner);
+                    //when we are combatting with enemy, we survive or die
+                    if (!survived) {
+                        System.out.println("you died, game over");
+                        break;
+                    }
+
+                    newLocation.clearEnermy();
+                    
+
+                }
+            }           
+            
+        }
+        scanner.close();
+    }
+
+    //generate a map of 5*5
+    private Location[][] generateMap() {
+        String[][] rawMap = {
+        {"x", "x", "cristal cave", "broken bridge", "temple of light"},
+        {"x", "x", "howling cliff", "x", "x"},
+        {"x", "desert", "forrest", "misty swamp", "x"},
+        {"beach", "lost village", "x", "everdark woods", "x"},
+        {"x", "x", "x", "frozen lake", "mystic library"}
+    };
+
+    Location[][] map = new Location[MAP_SIZE][MAP_SIZE];
+
+    for (int r = 0; r < MAP_SIZE; r++) {
+        for (int c = 0; c < MAP_SIZE; c++) {
+            String name = rawMap[r][c];
+            if (name.equals("x")) {
+                map[r][c] = new Location("This area does not exist", true);
+            } else {
+                map[r][c] = new Location(name, false);
             }
         }
     }
+
+        return map;
+    }
+
+
+    private void printMap() {
+        System.out.println("Map:");
+
+    for (int r = 0; r < MAP_SIZE; r++) {
+        for (int c = 0; c < MAP_SIZE; c++) {
+            if (player.getRow() == r && player.getCol() == c) {
+                System.out.print("P "); // player location
+            } else if (map[r][c].isImpossible()) {
+                System.out.print("X "); // not exist
+            } else {
+                System.out.print(". "); // exist
+            }
+        }
+        System.out.println(); 
+    }
+
+    }
+    private void printInventory() {
+        System.out.println("Inventory: ");
+        if (player.getInventory().isEmpty()) {
+        System.out.println("  (empty)");
+        } else {
+        for (String item : player.getInventory()) {
+            System.out.println("  - " + item);
+        }
+    }
+    }
+
+    //get items depend on the location ***we will change the compass item once we set the NPC system.
+    private void giveItemIfAvailable(String locationName) {
+    switch (locationName) {
+        case "forrest":
+            if (!player.getInventory().contains("compass")) {
+                player.addItem("compass");
+                System.out.println("You found a compass!");
+            }
+            if (!player.getInventory().contains("sword")) {
+                player.addItem("sword");
+                System.out.println("You found a sword!");
+            }
+            break;
+        case "beach":
+            if (!player.getInventory().contains("key")) {
+                player.addItem("key");
+                System.out.println("You found a key!");
+            }
+            break;
+        case "frozen lake":
+            if (!player.getInventory().contains("axe")) {
+                player.addItem("axe");
+                System.out.println("You found an axe!");
+            }
+            break;
+        case "mystic library":
+            if (!player.getInventory().contains("branch")) {
+                player.addItem("branch");
+                System.out.println("You found a mysterious branch!");
+            }
+            break;
+    }
+}
 }
--- src/game/entity/Player.java
+++ src/game/entity/Player.java
@@ -1,21 +1,25 @@
 package game.entity;
 /**
  * Player is a class that represent a user player
- * @seanlee1991
+ * @author Sanggyun Lee
  */
 public class Player {
     private int row;
     private int col;
     private double hp;
+    private double attack;
 
     public Player(int startRow, int startCol) {
         this.row = startRow;
         this.col = startCol;
         this.hp = 100.0;
+        this.attack = 10.0;
     }
+
     public int getRow() { return this.row;}
     public int getCol() { return this.col;}
     public double getHp() { return this.hp;}
+    public double getAttack() { return this.attack;}
     
     //this is to update the player's position after a move
     public void setPosition(int newRow, int newCol) {
@@ -24,6 +28,10 @@ public class Player {
     }
 
     public void setHp(double damageTaken) { this.hp -= damageTaken; }
+
+    public void getAttack(double additionalAttack) { this.attack += additionalAttack;}
+
+    public boolean isAlive() { return this.hp > 0.0;}
 
     }
 
